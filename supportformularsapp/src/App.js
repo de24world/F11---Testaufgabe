ee781e78a6cff91890024895fbe535f89143eb6f
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-25 21:36:55 +0100

    basic form input created

--- supportformularsapp/src/App.js
+++ supportformularsapp/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, errors } = useForm({});
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <h1> Supportformulars</h1>
+      <label>Name</label>
+      <input name="Name" ref={register({ required: true })} />
+      {errors.Name && errors.Name.type === "required" && (
+        <p>Befehle benötigen</p>
+      )}
+
+      <label>E-mail</label>
+      <input name="Email" ref={register({ required: true })} />
+      {errors.Email && errors.Email.type === "required" && (
+        <p>Befehle benötigen</p>
+      )}
+
+      <label>Theme Bereich</label>
+      <select name="Topic" ref={register({ required: true })}>
+        <option value="">Auswählen</option>
+        <option value="allgemeine">Allgemeine</option>
+        <option value="softwarefehler">Softwarefehler</option>
+        <option value="rückruf">Rückruf</option>
+      </select>
+      {errors.Topic && errors.Topic.type === "required" && (
+        <p>Befehle benötigen</p>
+      )}
+
+      <label>Beschreibung</label>
+      <textarea name="Description" ref={register({ required: true })} />
+      {errors.Description && errors.Description.type === "required" && (
+        <p>Befehle benötigen</p>
+      )}
+
+      <input type="submit" ref={register({ required: true })} />
+    </form>
   );
 }
 
