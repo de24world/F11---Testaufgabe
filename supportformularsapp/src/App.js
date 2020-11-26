e4ce739f34440719c3e466b8a45ae26905a845b3
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-26 16:33:48 +0100

    showVersion add

--- supportformularsapp/src/App.js
+++ supportformularsapp/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
 
-function App() {
-  const { register, handleSubmit, errors } = useForm({});
+function App(props) {
+  const { register, handleSubmit, watch, errors } = useForm({});
+  const showVersion = watch("selectd", props.selected);
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -10,13 +11,11 @@ function App() {
   return (
     <form className="App" onSubmit={handleSubmit(onSubmit)}>
       <h1> Supportformulars</h1>
-
       <label>Name</label>
       <input name="name" ref={register({ required: true })} />
       {errors.name && errors.name.type === "required" && (
         <p>Befehle benötigen</p>
       )}
-
       <label>E-mail</label>
       <input
         type="email"
@@ -30,9 +29,8 @@ function App() {
         })}
       />
       {errors.email && <p className="error">{errors.email.message}</p>}
-
       <label>Theme Bereich</label>
-      <select name="topic" ref={register({ required: true })}>
+      <select name="topic" name="selectd" ref={register({ required: true })}>
         <option value="">Auswählen</option>
         <option value="allgemeine">Allgemeine</option>
         <option value="softwarefehler">Softwarefehler</option>
@@ -42,38 +40,30 @@ function App() {
         <p>Befehle benötigen</p>
       )}
 
-      {/* if softwarefehler 문써서 보이게끔 */}
-      <label>Version</label>
-      <input
-        name="version"
-        placeholder="Version : X.X.X"
-        ref={register({
-          required: "Enter your Version",
-          pattern: {
-            value: /^[0-9]*\.[0-9]*\.[0-9]{1,2}$/,
-            message: "Nur x.x.x!!! //  x = Nummer // z.B. 1.0.0 oder 1.11.12",
-          },
-        })}
-      />
-      {errors.version && <p className="error">{errors.version.message}</p>}
-
-      {/* if rückruf 문써서 보이게끔 */}
-      <label>Telefonnummer</label>
-      <input
-        name="telefon"
-        placeholder="Telefonnummer"
-        ref={register({
-          required: "Enter your telefon",
-        })}
-      />
-      {errors.telefon && <p className="error">{errors.telefon.message}</p>}
+      {showVersion === "softwarefehler" && (
+        <>
+          <label>Version</label>
+          <input
+            name="version"
+            placeholder="Version : X.X.X"
+            ref={register({
+              required: "Enter your Version",
+              pattern: {
+                value: /^[0-9]*\.[0-9]*\.[0-9]{1,2}$/,
+                message:
+                  "Nur x.x.x!!! //  x = Nummer // z.B. 1.0.0 oder 1.11.12",
+              },
+            })}
+          />
+          {errors.version && <p className="error">{errors.version.message}</p>}
+        </>
+      )}
 
       <label>Beschreibung</label>
       <textarea name="Description" ref={register({ required: true })} />
       {errors.Description && errors.Description.type === "required" && (
         <p>Befehle benötigen</p>
       )}
-
       <input type="submit" ref={register({ required: true })} />
     </form>
   );
