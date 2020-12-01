6058ad7132ef57c0dea761577fe6f57e8cccf6d4
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-01 21:47:06 +0100

    useSuspense error fix

--- supportformularsapp/src/locales/i18next.js
+++ supportformularsapp/src/locales/i18next.js
@@ -31,6 +31,9 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    react: {
+      useSuspense: false,
+    },
     resources: resource,
     fallbackLng: "en",
     debug: true,
